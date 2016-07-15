d7ba5eeff3111ee24c8fa0e3ab1c2128d4de0325
leapon <leapon@gmail.com>
2016-07-14 20:07:28 -0400

    added hello component in folder;clean unused components

---
+++ site/jsbuild/src/components/hello/main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello_container">
+    <h2 class="hello_message">{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    msg: 'Hello'
+  }
+}
+</script>
+
+<style>
+.hello_container {
+  border: 1px solid #00f;
+}
+.hello_message {
+  color: #f00;
+}
+</style>
