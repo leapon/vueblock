142a66e9386e9abaccd4cc099938472ecb39a3ff
leapon <leapon@gmail.com>
2016-07-16 21:54:36 -0400

    add doorlayout component

---
+++ site/jsbuild/src/component/doorlayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div>left</div>
+      </div>
+      <div class="col-md-6">
+        <div>{{ content }}</div>
+      </div>
+      <div class="col-md-3">
+        <div>right</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['showLeft', 'showRight'],
+  data () {
+    return {
+      content: 'ipsum Lorem ipsum dolor'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- site/jsbuild/src/component/hello.vue
+++ site/jsbuild/src/component/hello.vue
@@ -11,7 +11,7 @@ export default {
     return {
       greeting: 'Hello'
     }
-  },
+  }
 }
 </script>
 
