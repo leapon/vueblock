fc4d28900f2d4670e37d0df5c2e04aacdec2fb23
leapon <leapon@gmail.com>
2016-07-17 09:27:39 -0400

    remove user related UI on web page

--- site/setting.js
+++ site/setting.js
@@ -2,10 +2,5 @@ exports.setting = {
   // web
   http_port: 8010,
   // database
-  database: {
-    type: 'mongo',
-    port: 27017,
-    host: 'localhost',
-    name: 'vueblock'
-  }
+  database: null
 };
